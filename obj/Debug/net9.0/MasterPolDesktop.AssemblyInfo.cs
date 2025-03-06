6ddc9fda79855911d8b23bb79a821c8371e9e279
ArrayKat <kolinichenko.05@mail.ru>
2025-03-07 02:03:29 +0300

    feat: and dll and UnitTests

--- obj/Debug/net9.0/MasterPolDesktop.AssemblyInfo.cs
+++ obj/Debug/net9.0/MasterPolDesktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterPolDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d008f722adc02787b312f70d847134914f50d5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7e6c6da8c1a19ce965aa690eb001e32ca81f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterPolDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterPolDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
